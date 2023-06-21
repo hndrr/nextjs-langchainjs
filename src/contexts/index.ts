d4e189879ca147026f55f36b7cac962ea7be1e0a
hndrr <yozhik20@gmail.com>
2023-06-21 21:22:16 +0900

    fix: MutationContextTypeを修正

--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -2,8 +2,13 @@ import { UseMutationResult } from "@tanstack/react-query";
 import React from "react";
 
 type MutationContextType = {
-  mutate: (data?: any, variables?: any, context?: any, options?: any) => void;
-  isLoading: boolean;
+  mutate: UseMutationResult<string, unknown, string, unknown>["mutate"];
+  isLoading: UseMutationResult<string, unknown, string, unknown>["isLoading"];
+  isError: UseMutationResult<string, unknown, string, unknown>["isError"];
+  isSuccess: UseMutationResult<string, unknown, string, unknown>["isSuccess"];
+  error: UseMutationResult<string, unknown, string, unknown>["error"];
+  data: UseMutationResult<string, unknown, string, unknown>["data"];
+  reset: UseMutationResult<string, unknown, string, unknown>["reset"];
 };
 
 export const MutationContext = React.createContext<MutationContextType | null>(
